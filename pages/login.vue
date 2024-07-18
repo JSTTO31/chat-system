90f92cbf9ebc679eff9f98a00defe7af0c3587bc
Joshua Sotto <joshuasottoelyen@gmail.com>
2024-07-19 04:21:37 +0800

    loading indicator

--- pages/login.vue
+++ pages/login.vue
@@ -1,7 +1,7 @@
 <template>
     <v-main class="d-flex align-center justify-center bg-primary">
         <v-form @submit.prevent="submit">
-            <v-card width="550" class="pa-5 rounded-lg">
+            <v-card width="550" class="pa-5 rounded-lg" :disabled="loading">
                 <div class="pa-5 d-flex">
                     <v-avatar size="95" class="mx-auto">
                         <v-img src="/logo.png"></v-img>
@@ -14,7 +14,7 @@
                     <v-text-field id="password" variant="outlined" single-line type="password" :error-messages="$v.password.$errors[0]?.$message.toString() || ''" v-model="$v.password.$model" label="Password"></v-text-field>
                 </v-card-text>
                 <v-card-actions>
-                    <v-btn block type="submit" color="primary" class="text-capitalize" variant="elevated">Login</v-btn>
+                    <v-btn block :loading="loading" type="submit" color="primary" class="text-capitalize" variant="elevated">Login</v-btn>
                 </v-card-actions>
             </v-card>
         </v-form>
@@ -31,7 +31,7 @@ definePageMeta({
     middleware: ['guest'],
 })
 const router = useRouter()
-
+const loading = ref(false)
 const $user = useUserStore()
 const info = reactive({
     email: 'joshuasotto@example.example',
@@ -47,9 +47,11 @@ const $v = useVuelidate(rules, info, {$externalResults})
 
 const submit = async () => {
     if(!await $v.value.$validate()) return
+    loading.value = true
     //@ts-ignore
     const {error} = await $user.login(info)
-    console.log(error.value);
+    loading.value = false
+    
     
     if(error.value){
         for(const [key, value] of Object.entries(error.value.data.errors)){
